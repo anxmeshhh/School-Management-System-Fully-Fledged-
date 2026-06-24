f86a351787d8256eca430dceb3867901853f2bbb
anxmeshhh <theanimeshgupta@gmail.com>
2026-06-24 09:45:58 +0530

    Add global favicon.ico routing and explicitly link favicon in admin_base.html

--- school_management/urls.py
+++ school_management/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-
 from django.views.generic import TemplateView
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('sw.js', TemplateView.as_view(template_name='users/sw.js', content_type='application/javascript'), name='sw.js'),
+    path('favicon.ico', RedirectView.as_view(url='/static/users/images/adminlogo.jpg', permanent=True)),
     path('', include('users.urls')), 
 ]
